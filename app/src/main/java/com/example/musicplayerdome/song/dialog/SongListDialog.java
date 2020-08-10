c4ea357fc0365e49f519eed1827a13208fe2d173
brokes6 <619199091@qq.com>
2020-08-10 14:21:01 +0800

    对弹窗进行一些小修改（修复了弹窗会引出虚拟按键的BUG）

--- app/src/main/java/com/example/musicplayerdome/song/dialog/SongListDialog.java
+++ app/src/main/java/com/example/musicplayerdome/song/dialog/SongListDialog.java
@@ -265,4 +265,16 @@ public class SongListDialog extends Dialog implements SongContract.View,View.OnC
     public void onGetPlaylistCommentFail(String e) {
 
     }
+
+    /**
+     * 重写show方法，将高度设置为全屏高度，取消谈起虚拟按键
+     */
+    public void show() {
+        if (this.getWindow() != null) {
+            this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE);
+            this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            super.show();
+            this.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE);
+        }
+    }
 }
